ff24ec51665e6d7b8097a010dfccfc6ece823ece
NonRn <rn.non@hotmail.com>
2018-04-02 16:52:42 +0700

    [Edit] FlowRepository for use in ListFlowController

--- app/Repositories/Eloquent/EloquentFlowRepository.php
+++ app/Repositories/Eloquent/EloquentFlowRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories\Eloquent;
 
 use App\Models\Flow;
 use App\Repositories\Contracts\FlowRepository;
-use App\Repositories\Eloquent\EloquentCategoryRepository ;
+use App\Repositories\Eloquent\EloquentCategoryRepository;
 use Kurt\Repoist\Repositories\Eloquent\AbstractRepository;
 
 class EloquentFlowRepository extends AbstractRepository implements FlowRepository
@@ -14,19 +14,21 @@ class EloquentFlowRepository extends AbstractRepository implements FlowRepositor
         return Flow::class;
     }
 
-    public function listFlow(){
+    public function listFlow()
+    {
         $data = Flow::all();
-        return json_decode($data);    
+        return json_decode($data);
     }
 
-    public static function getFlowGroupCat(){
+    public static function getFlowGroupCat()
+    {
         $allCat = EloquentCategoryRepository::getAllCategory();
-        $flowGroupCat = array() ;
-        foreach($allCat as $cat){
+        $flowGroupCat = array();
+        foreach ($allCat as $cat) {
             $data = Flow::where('flow_CatId', '=', $cat['cat_Id'])->get();
-            $data = json_decode($data,true);
-            $flowGroupCat = array_add($flowGroupCat, $cat['cat_Id'],$data);
+            $data = json_decode($data, true);
+            $flowGroupCat = array_add($flowGroupCat, $cat['cat_Name'], $data);
         }
-        return $flowGroupCat ;
+        return $flowGroupCat;
     }
 }
