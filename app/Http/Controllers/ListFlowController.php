e015cdefed77f467c5ccfd25d823649258c29ff9
NonRn <rn.non@hotmail.com>
2018-04-02 16:53:24 +0700

    [Add] ListFlowController

---
+++ app/Http/Controllers/ListFlowController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Eloquent\EloquentFlowRepository as Flow;
+class ListFlowController extends Controller
+{
+    public function listFlow(){
+        $data = Flow::getFlowGroupCat();
+        return view('listflow',['allFlow'=>$data]);
+    }
+}
