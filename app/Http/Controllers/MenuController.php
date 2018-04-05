9c0c2e3ed879b54243837211a0d5d740747d2098
patchareeporn-sircharoen <patchareeporn.scr@gmail.com>
2018-04-05 19:42:41 +0700

    [Edit] getMenu function in MenuController and route

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -7,11 +7,12 @@ use App\Repositories\Eloquent\EloquentUserRepository as UserRepo;
 
 class MenuController extends Controller
 {
-    public function getMenu($id)
+    public function getMenu(Request $request)
     {
+        $id = $request->input('id');
         $data = UserRepo::getUser($id);
         if($data->user_Role == "manager"){
-            return view('manager_menu');
+            return view('manager_menu',['data'=>$data]);
         }else
             return view('user_menu');
     }
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::any('listflow', 'ListFlowController@listFlow');
+
+Route::get('/', 'MenuController@getMenu');
