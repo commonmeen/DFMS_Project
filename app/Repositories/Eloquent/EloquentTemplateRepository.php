48f761d436a37dc1962dab8aee87e0ac14aff711
NonRn <rn.non@hotmail.com>
2018-04-05 19:47:58 +0700

    [Add] Template Repository with listTemplate method

---
+++ app/Repositories/Eloquent/EloquentTemplateRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Template;
+use App\Repositories\Contracts\TemplateRepository;
+
+use Kurt\Repoist\Repositories\Eloquent\AbstractRepository;
+
+class EloquentTemplateRepository extends AbstractRepository implements TemplateRepository
+{
+    public function entity()
+    {
+        return Template::class;
+    }
+
+    public static function listTemplate(){
+        $data = Template::all();
+        return json_decode($data,true);
+    }
+}
