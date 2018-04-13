fcc9fcb1829937dbb8521f1baf468ad2f10f6c8f
NonRn <rn.non@hotmail.com>
2018-04-13 20:35:53 +0700

    [Add] AddFlowController

---
+++ app/Http/Controllers/AddFlowController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Eloquent\EloquentFlowRepository as Flow ;
+use App\Repositories\Eloquent\EloquentTemplateRepository as Template;
+
+class AddFlowController extends Controller
+{
+    public function addFlow(Request $request){
+        $input = $request->all();
+        $newFlowId = Flow::addFlow($input['name'],$input['author'],$input['desc'],$input['catId'],$input['deadline'],$input['numberOfStep']);
+        $allTemplate = Template::listTemplate();
+        return view('AddFlowTemplate',['newId'=>$newFlowId,'template'=>$allTemplate]);
+    }
+}
