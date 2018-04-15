4b459826b580ee7bf3b6ba4815a6534012b1fff4
commonmeen <commonmeen@gmail.com>
2018-04-16 01:41:54 +0700

    [Edit] View ListTemplate

--- resources/views/listtemplate.blade.php
+++ resources/views/listtemplate.blade.php
@@ -1,94 +1,81 @@
 @extends('layout.navbar')
 @section('head')
-    
+    <link type="text/css" href="../css/AddFlow.css" rel="stylesheet">
 @endsection
 @section('content')
     <div class="container content">
-        <h3 class="lb-txt-bold lb-txt-uppercase lb-txt-18 lb-h3 lb-title">
-            Create Flow
-        </h3>
-        <div class="row">
-            <div class="col-lg-10 col-sm-6">
-                <div class="d-none d-md-block">
-                    <a class="btn btn-outline-primary" href="#" role="button">Detail flow</a>
-                    <a class="btn btn-primary" href="" role="button">Select template</a>
-                    @for($i=1;$i<=3;$i++)
-                        <a class="btn btn-outline-primary" href="#" role="button">Step {{$i}}</a>
-                    @endfor
-                </div>
-                <div class="d-md-none">
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Menu
-                            </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Detail flow</a>
-                            <a class="dropdown-item" href="">Select template</a>
-                            @for($i=1;$i<=3;$i++)
-                                <a class="dropdown-item" href="#">step{{$i}}</a>
-                            @endfor
-                            <a class="dropdown-item" href="#">Create</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-sm-6">
-                <div class="d-none d-md-block">    
-                    <button type="button" class="btn btn-primary button-position">Create</button>
-                </div> 
-            </div>
-            
-        </div>
+        <form action="AddFlowTemplate">
 
+        {{-- Large Screen --}}
+    <div class="d-none d-sm-block">
         <div class="row">
-            @for($i=1;$i<=7;$i++)
-            <div class="col-md-4 col-lg-3 content">
-                <img src="pic/user.png" class="card-img-top tempImg">
-                <div class="card">
-                    <div class="card-body" style="text-align:center">Template {{$i}}</div>
-                </div>
+            <div class="col">
+                <h3>Create Flow</h3>
             </div>
-            @endfor
+            <div class="col">
+                <input type="submit" class="btn btn-success float-right " value="Next">
+                <a role="button" class="btn btn-primary float-right mr-2" href="AddTemplate">Create</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="btn btn-outline-secondary" href="" role="button">Detail flow</a>
+                <a class="btn btn-secondary" href="#" role="button" >Select template</a>
+                @for($i=1;$i<=$Flow['numberOfStep'];$i++)
+                    <a class="btn btn-outline-secondary" href="" role="button" >Step {{$i}}</a>
+                @endfor
+            </div>
         </div>
     </div>
 
-    {{-- <div class="row">
-        <div>
-            <div class="d-none d-md-block">
-                <a class="btn btn-outline-primary" href="#" role="button">Detail flow</a>
-                <a class="btn btn-primary" href="" role="button">Select template</a>
-                @for($i=1;$i<=3;$i++)
-                    <button type="button" class="btn btn-outline-primary">step{{$i}}</button>
-                @endfor
+    {{-- Small Screen --}}
+    <div class="d-sm-none">
+        <div class="row">
+            <div class="col-12">
+                <p class="top-menu text-center">Create Flow</p>
             </div>
-            <div class="d-sm-none">
-                <div class="dropdown">
+        </div>
+        <div class="row">
+            <div class="col-4 ">
+                <div class="dropbown">
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown button
+                        Menu
                     </button>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                         <a class="dropdown-item" href="#">Detail flow</a>
-                        <a class="dropdown-item" href="#">Select template</a>
-                        @for($i=1;$i<=3;$i++)
-                            <a class="dropdown-item" href="#">step{{$i}}</a>
-                        @endfor 
+                        <a class="dropdown-item" href="" >Select template</a>
+                        @for($i=1;$i<=$Flow['numberOfStep'];$i++)
+                            <a class="dropdown-item" href="#">Step {{$i}}</a>
+                        @endfor
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn btn-primary button-position">Create</button>
-        
+            <div class="col-8">
+                <input type="submit" class="btn btn-success  float-right" value="Next">
+                <a role="button" class="btn btn-primary float-right mr-2" href="AddTemplate">Create</a>
+            </div>         
+        </div>   
+    </div>        
+            
+    
+        <div class="row">
+                @foreach($template as $t )
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 content">
+                    <input class="c-card" type="checkbox" id="{{$t['template_Id']}}" value="{{$t['template_Id']}}" name="template_Id[]" >
+                        <div class="card-content">
+                            <div class="card-state-icon"></div>
+                            <label for="{{$t['template_Id']}}">
+                                <img src="pic/user.png" alt="{{$t['template_Name']}}" class="card-img-top tempImg">
+                                <div class="card-body ">
+                                    <p class="text-center font-weight-bold ">{{$t['template_Name']}}</p>
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <input type="text" value={{$Flow['flow_Id']}} hidden name="flow_Id">
+        </form>
     </div>
 
-    <div class="row">
-        @for($i=1;$i<=8;$i++)
-        <div class="col-md-4 col-lg-2 content">
-            <img src="pic/user.png" class="card-img-top tempImg">
-            <div class="card">
-            <div class="card-body" style="text-align:center">Template {{$i}}</div>
-            </div>
-        </div>
-        @endfor
-    </div> --}}
-
-    
 @endsection
