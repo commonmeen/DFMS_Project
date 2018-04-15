ed670a4fe4bec2c4d0e93957d0830f79c5891a56
NonRn <rn.non@hotmail.com>
2018-04-15 17:41:05 +0700

    [Add] Controller AddFlowTemplateController

---
+++ app/Http/Controllers/AddFlowTemplateController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Eloquent\EloquentFlowRepository as Flow ;
+use App\Repositories\Eloquent\EloquentUserRepository as User ;
+
+class AddFlowTemplateController extends Controller
+{
+    public function addFlowTemlate(Request $request){
+        $input = $request->all();
+        Flow::addFlowTemplate($input['flow_Id'],$input['template_Id']);
+        $next = 1 ;
+        $allUser = User::listUser();
+        return view('AddStep',['step'=>$next,'userList'=>$allUser]) ;
+    }
+}
