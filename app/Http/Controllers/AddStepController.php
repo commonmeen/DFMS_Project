2737210734585d8c831b9ad25faaffb608b44c8a
NonRn <rn.non@hotmail.com>
2018-04-16 01:33:29 +0700

    [Add] Controller AddStepController

---
+++ app/Http/Controllers/AddStepController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Eloquent\EloquentStepRepository as step ;
+
+class AddStepController extends Controller
+{
+    public function addStep(Request $request){
+        $input = $request->all();
+        step::addStep($input['title'],$input['type'],$input['flow_id'],$input['validator'],$input['deadline']);
+        $flow = $input['flow'];
+        if($input['next']==$flow['numberOfStep']){
+            return view('listFlow') ;
+        } else {
+            $next = $input['next']+1 ;
+            $allUser = User::listUser();
+            return view('AddStep',['step'=>$next,'userList'=>$allUser,'Flow'=>$flow]) ;
+        }
+    }
+}
