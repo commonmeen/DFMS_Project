4aeab74ff010947c75c23d4fde8a145f9129c820
NonRn <rn.non@hotmail.com>
2018-04-17 13:23:54 +0700

    [Edit] Controller AddFlowController | Set Flow Session

--- app/Http/Controllers/AddFlowController.php
+++ app/Http/Controllers/AddFlowController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session ;
 use App\Repositories\Eloquent\EloquentFlowRepository as Flow ;
 use App\Repositories\Eloquent\EloquentTemplateRepository as Template;
 
@@ -10,8 +11,10 @@ class AddFlowController extends Controller
 {
     public function addFlow(Request $request){
         $input = $request->all();
-        $newFlowId = Flow::addFlow($input['name'],$input['author'],$input['desc'],$input['catId'],$input['deadline'],$input['numberOfStep']);
+        $user = Session::get('UserLogin');
+        $newFlowId = Flow::addFlow($input['name'],$user->user_Id,$input['desc'],$input['catId'],$input['deadline'],$input['numberOfStep']);
         $thisFlow = Flow::getFlowById($newFlowId);
+        Session::put('FlowCreate',$thisFlow);
         $allTemplate = Template::listTemplate();
         return view('ListTemplate',['Flow'=>$thisFlow,'template'=>$allTemplate]);
     }
