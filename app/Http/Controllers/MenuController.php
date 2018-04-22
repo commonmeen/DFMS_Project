d7b6f920dcf98a1bb2952a366e66bbb999ef01ac
commonmeen <commonmeen@gmail.com>
2018-04-22 18:44:30 +0700

    [Edit] Controller MenuController

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -10,14 +10,12 @@ class MenuController extends Controller
 {
     public function getMenu(Request $request)
     {
-        $id = $request->input('id');
+        $id = 'U00001';
+        // $id = $request->input('id');
         $data = UserRepo::getUser($id);
         if($data!=null){
             Session::put('UserLogin',$data);
-            if($data->user_Role == "manager"){
-                return view('manager_menu',['data'=>$data]);
-            }else
-                return view('user_menu');
+            return view('Home',['data'=>$data]);
         }
         return ;
     }
