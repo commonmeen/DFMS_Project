a3616cfc5f518e8f82e9b442f92bdb63686e8cf2
NonRn <rn.non@hotmail.com>
2018-04-29 19:52:49 +0700

    [Edit] Controller AddStepController | Simple add step

--- app/Http/Controllers/AddStepController.php
+++ app/Http/Controllers/AddStepController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session ;
 use App\Repositories\Eloquent\EloquentStepRepository as step ;
 
 class AddStepController extends Controller
 {
     public function addStep(Request $request){
         $input = $request->all();
-        step::addStep($input['title'],$input['type'],$input['flow_id'],$input['validator'],$input['deadline']);
-        $flow = $input['flow'];
-        if($input['next']==$flow['numberOfStep']){
-            return view('ListFlow') ;
-        } else {
-            $next = $input['next']+1 ;
-            $allUser = User::listUser();
-            return view('AddStep',['step'=>$next,'userList'=>$allUser,'Flow'=>$flow]) ;
-        }
+        $flow = Session::get('FlowCreate');
+        step::addStep($input['title'],$input['type'],$flow['flow_Id'],$input['validator'],$input['deadline']);
+        // $flow = $input['flow'];
+        // if($input['next']==$flow['numberOfStep']){
+        return    redirect('ListFlow');
+        // } else {
+        //     $next = $input['next']+1 ;
+        //     $allUser = User::listUser();
+        //     return view('AddStep',['step'=>$next,'userList'=>$allUser,'Flow'=>$flow]) ;
+        // }
     }
 }
