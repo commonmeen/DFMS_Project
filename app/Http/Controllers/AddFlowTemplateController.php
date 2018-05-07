1256a5d02b681288fb54eaba2abbfe9e14ac0394
NonRn <rn.non@hotmail.com>
2018-05-07 17:46:21 +0700

    [Edit] Controller AddFlowTemplateController | new pettern of position

--- app/Http/Controllers/AddFlowTemplateController.php
+++ app/Http/Controllers/AddFlowTemplateController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\Eloquent\EloquentFlowRepository as Flow ;
-use App\Repositories\Eloquent\EloquentUserRepository as User ;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo ;
+use App\Repositories\Eloquent\EloquentUserRepository as userRepo ;
+use App\Repositories\Eloquent\EloquentPositionRepository as positionRepo ;
 
 class AddFlowTemplateController extends Controller
 {
     public function addFlowTemplate(Request $request){
         $input = $request->all();
-        Flow::addFlowTemplate($input['flow_Id'],$input['template_Id']);
+        if ($request->has('template_Id')){
+            flowRepo::addFlowTemplate($input['flow_Id'],$input['template_Id']);
+        }
         $next = 1 ;
-        $allUser = User::listUser();
-        $position = User::getPosition();
+        $allUser = userRepo::listUser();
+        $position = positionRepo::getAllPosition();
         return view('AddStep',['step'=>$next,'userList'=>$allUser, 'userPosition'=>$position]) ;
     }
 }
