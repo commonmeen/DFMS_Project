d49b5eb0113b97d124dad21495144ead33b10c99
NonRn <rn.non@hotmail.com>
2018-05-07 18:35:39 +0700

    [Edit] Controller SearchUserController | search by position

--- app/Http/Controllers/SearchUserController.php
+++ app/Http/Controllers/SearchUserController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\Eloquent\EloquentUserRepository as user;
+use App\Repositories\Eloquent\EloquentUserRepository as userRepo;
 
 class SearchUserController extends Controller
 {
     public function searchUser(Request $request){
         $input = $request->all();
-        $name = user::searchByName($input['search']);
-        $surname = user::searchBySurname($input['search']);
+        $name = userRepo::searchByName($input['search']);
+        $surname = userRepo::searchBySurname($input['search']);
         $all = array_collapse([$name,$surname]);
         $all = array_unique($all,0);
         return ['searchAll'=>$all] ;
+    }
+
+    public function searchPosition(Request $request){
+        $input = $request->all();
+        $user = userRepo::listUserByPosition($input['position_Id']);
+        return ['searchAll'=>$user] ;
     }
 
 }
