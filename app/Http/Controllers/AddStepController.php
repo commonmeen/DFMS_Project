902bd9c363f8ee7dd0c81c6871f40eaaaad94cbb
NonRn <rn.non@hotmail.com>
2018-05-10 10:49:10 +0700

    [Edit] Controller AddStepController | validator with position

--- app/Http/Controllers/AddStepController.php
+++ app/Http/Controllers/AddStepController.php
@@ -6,19 +6,27 @@ use Illuminate\Http\Request;
 use Session ;
 use App\Repositories\Eloquent\EloquentStepRepository as stepRepo ;
 use App\Repositories\Eloquent\EloquentUserRepository as userRepo ;
+use App\Repositories\Eloquent\EloquentPositionRepository as positionRepo ;
 
 class AddStepController extends Controller
 {
     public function addStep(Request $request){
         $input = $request->all();
         $flow = Session::get('FlowCreate');
-        stepRepo::addStep($input['title'],$input['type'],$flow['flow_Id'],$input['validator'],$input['deadline']);
+        // check type of validator
+        if($input['selectBy']=="search"){
+            stepRepo::addStep($input['title'],$input['type'],"name",$flow['flow_Id'],$input['validator'],$input['deadline']);
+        } else if($input['selectBy']=="position"){
+            stepRepo::addStep($input['title'],$input['type'],"position",$flow['flow_Id'],[$input['position']],$input['deadline']);
+        }
+        // check step number
         if($input['step']==$flow['numberOfStep']){
+            Session::forget('FlowCreate');
             return    redirect('ListFlow');
         } else {
             $next = $input['step']+1 ;
             $allUser = userRepo::listUser();
-            $position = userRepo::getPosition();
+            $position = positionRepo::getAllPosition();
             return view('AddStep',['step'=>$next,'userList'=>$allUser, 'userPosition'=>$position]) ;
         }
     }
