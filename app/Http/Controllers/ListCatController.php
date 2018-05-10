838853ea56029eb3639829d5b8bcbd8058194dd4
NonRn <rn.non@hotmail.com>
2018-05-10 23:57:42 +0700

    [Edit] Controller ListCatController | Support Edit Flow

--- app/Http/Controllers/ListCatController.php
+++ app/Http/Controllers/ListCatController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\Eloquent\EloquentCategoryRepository as Category;
+use App\Repositories\Eloquent\EloquentCategoryRepository as categoryRepo;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo;
 
 class ListCatController extends Controller
 {
-    public function getAllCategory()
+    public function getAllCategory(Request $request)
     {
-        $data = Category::getAllCategory();
-        return view('AddFlow',['listCat'=>$data]);
+        $input = $request->all();
+        $data = categoryRepo::getAllCategory();
+        $flow = null ;
+        if($request->has('flow_Id')){
+            $flow = flowRepo::getFlowById($input['flow_Id']);
+        }
+        return view('AddFlow',['listCat'=>$data,'flow'=>$flow]);
     }
 }
