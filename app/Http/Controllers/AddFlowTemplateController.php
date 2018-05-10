1bd7af46561cd076e6ed80eb6f4065eeee439a12
NonRn <rn.non@hotmail.com>
2018-05-10 23:56:53 +0700

    [Edit] Controller AddFlowTemplateController | Support Edit Flow

--- app/Http/Controllers/AddFlowTemplateController.php
+++ app/Http/Controllers/AddFlowTemplateController.php
@@ -12,13 +12,20 @@ class AddFlowTemplateController extends Controller
 {
     public function addFlowTemplate(Request $request){
         $input = $request->all();
-        $flow = Session::get('FlowCreate');
-        if ($request->has('template_Id')){
-            flowRepo::addFlowTemplate($flow["flow_Id"],$input['template_Id']);
+        if($request->has('flow_Id')){
+            if ($request->has('template_Id')){
+                flowRepo::addFlowTemplate($input["flow_Id"],$input['template_Id']);
+            }
+            return redirect()->route('FlowDetail',['id'=>$input["flow_Id"]]);
+        }else{
+            $flow = Session::get('FlowCreate');
+            if ($request->has('template_Id')){
+                flowRepo::addFlowTemplate($flow["flow_Id"],$input['template_Id']);
+            }
+            $next = 1 ;
+            $allUser = userRepo::listUser();
+            $position = positionRepo::getAllPosition();
+            return view('AddStep',['step'=>$next,'userList'=>$allUser, 'userPosition'=>$position, 'flow'=>$flow]) ;
         }
-        $next = 1 ;
-        $allUser = userRepo::listUser();
-        $position = positionRepo::getAllPosition();
-        return view('AddStep',['step'=>$next,'userList'=>$allUser, 'userPosition'=>$position, 'flow'=>$flow]) ;
     }
 }
