f4b95279fa27a1207267822cd5d9e7e360ec3584
NonRn <rn.non@hotmail.com>
2018-06-07 02:51:20 +0700

    [Add] Controller EditStepController

---
+++ app/Http/Controllers/EditStepController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Eloquent\EloquentStepRepository as stepRepo ;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo ;
+use App\Repositories\Eloquent\EloquentUserRepository as userRepo ;
+use App\Repositories\Eloquent\EloquentPositionRepository as positionRepo ;
+
+class EditStepController extends Controller
+{
+    public function editStep(Request $request){
+        $input = $request->all();
+        $thisStep = stepRepo::getStepById($input['id']);
+        $flow = flowRepo::getFlowById($thisStep->flow_Id);
+        $allUser = userRepo::listUser();
+        $position = positionRepo::getAllPosition();
+        return view('AddStep',['step'=>null, 'userList'=>$allUser, 'userPosition'=>$position , 'flow'=>$flow, 'stepData'=>$thisStep]) ;
+    }
+}
