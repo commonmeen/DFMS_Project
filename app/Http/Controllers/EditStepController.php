90df6c1c4c76314759504d0d20e6ed4ee019ca44
NonRn <rn.non@hotmail.com>
2018-06-09 03:08:21 +0700

    [Edit] Controller EditStepController | Support step of create flow (turn back to edit step)

--- app/Http/Controllers/EditStepController.php
+++ app/Http/Controllers/EditStepController.php
@@ -13,11 +13,20 @@ class EditStepController extends Controller
 {
     public function editStep(Request $request){
         $input = $request->all();
-        $thisStep = stepRepo::getStepById($input['id']);
+        $thisStep = stepRepo::getStepById($input['id']);        
         $flow = flowRepo::getFlowById($thisStep['flow_Id']);
         $allUser = userRepo::listUser();
         $position = positionRepo::getAllPosition();
-        Session::put('stepEdit',$thisStep);
-        return view('AddStep',['step'=>null, 'userList'=>$allUser, 'userPosition'=>$position , 'flow'=>$flow, 'stepData'=>$thisStep]) ;
+        if(Session::has('FlowCreate')){
+            $allStep = stepRepo::getStepByFlow($flow['flow_Id']);
+            $allStepId = array();
+            foreach($allStep as $step){
+                array_push($allStepId,$step['step_Id']);
+            }
+            return view('AddStep',['allStep'=>$allStepId, 'step'=>$input['stepck'], 'userList'=>$allUser, 'userPosition'=>$position , 'flow'=>$flow, 'stepData'=>$thisStep]) ;
+        } else {
+            Session::put('stepEdit',$thisStep);
+            return view('AddStep',['step'=>null, 'userList'=>$allUser, 'userPosition'=>$position , 'flow'=>$flow, 'stepData'=>$thisStep]) ;
+        }
     }
 }
