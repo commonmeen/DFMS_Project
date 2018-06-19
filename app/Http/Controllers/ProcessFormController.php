c26d3d1942728aa829e5d4ab5bac334b20cb14c7
NonRn <rn.non@hotmail.com>
2018-06-20 02:39:38 +0700

    [Add] Controller ProcessFormController

---
+++ app/Http/Controllers/ProcessFormController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo;
+
+class ProcessFormController extends Controller
+{
+    public function newProcessForm(){
+        $flow = flowRepo::listFlowCanUse();
+        return view('NewProcess',['flow'=>$flow]);
+    }
+}
