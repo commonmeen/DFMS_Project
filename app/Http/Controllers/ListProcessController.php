e26f4fbd238403f10beb52aa3ac9d16ef3dcb8b2
NonRn <rn.non@hotmail.com>
2018-06-20 02:00:57 +0700

    [Add] Controller ListProcessController

---
+++ app/Http/Controllers/ListProcessController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session ;
+use App\Repositories\Eloquent\EloquentProcessRepository as processRepo ;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo ;
+
+class ListProcessController extends Controller
+{
+    public function listProcess(){
+        $user = Session::get('UserLogin');
+        $processes = processRepo::getProcessByOwner($user->user_Id); 
+        $processWithFlow = array();
+        foreach($processes as $process => $flow_Id){
+            $flow = flowRepo::getFlowById($flow_Id['process_FlowId']);
+            $flow_Id['process_Flow'] = $flow ;
+            array_push($processWithFlow,$flow_Id );
+        }
+        return view('ListProcess',['allProcess'=>$processWithFlow]);
+    }
+}
