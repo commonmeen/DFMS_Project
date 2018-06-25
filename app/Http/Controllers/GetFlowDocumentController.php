d472475b792f47f19337d722440f80329c227946
NonRn <rn.non@hotmail.com>
2018-06-26 03:49:15 +0700

    [Add] Controller GetFlowDocumentController

---
+++ app/Http/Controllers/GetFlowDocumentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session ;
+use App\Repositories\Eloquent\EloquentDocumentRepository as docRepo;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo;
+
+class GetFlowDocumentController extends Controller
+{
+    public function getDoc(Request $request){
+        $input = $request->all();
+        $flow = flowRepo::getFlowById($input['flow_Id']);
+        $user = Session::get('UserLogin');
+        $document = docRepo::listDocumentByUserId($user->user_Id);
+        $document = docRepo::filterDocumentByFlow($flow,$document);
+        return ['documentList'=>$document] ;
+    }
+}
