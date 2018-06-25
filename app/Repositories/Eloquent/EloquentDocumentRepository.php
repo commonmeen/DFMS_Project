4673bf85ac191f745d8e6cbb9d7f320c9a1ad62e
NonRn <rn.non@hotmail.com>
2018-06-26 03:48:35 +0700

    [Add] Method filterDocumentByFlow ; DocumentRepository

--- app/Repositories/Eloquent/EloquentDocumentRepository.php
+++ app/Repositories/Eloquent/EloquentDocumentRepository.php
@@ -20,7 +20,19 @@ class EloquentDocumentRepository extends AbstractRepository implements DocumentR
     }
 
     public static function listDocumentByUserId($userId){
-        $data = Document::where('document_Author',$user_Id)->get();
+        $data = Document::where('document_Author',$userId)->get();
         return json_decode($data,true);
+    }
+
+    public static function filterDocumentByFlow($flow,$documentList){
+        $documentCanUse = array();
+        foreach($flow['template_Id'] as $template){
+            foreach($documentList as $doc){
+                if($template == $doc['document_TemplateId']){
+                    array_push($documentCanUse,$doc);
+                }
+            }
+        }
+        return $documentCanUse ;
     }
 }
