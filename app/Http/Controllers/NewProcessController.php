b853072c6d0eee8e57a00c0b845d6e67d88281d8
NonRn <rn.non@hotmail.com>
2018-06-27 03:20:36 +0700

    [Add] Controller newProcessController

---
+++ app/Http/Controllers/NewProcessController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session ;
+use App\Repositories\Eloquent\EloquentProcessRepository as processRepo ;
+
+class NewProcessController extends Controller
+{
+    public function newProcess(Request $request){
+        $input = $request->all();
+        $user = Session::get('UserLogin');
+        dd($input);
+        if($input['file'][0] == null)
+            $input['file'] = [] ;
+        $process = processRepo::newProcess($input['name'],$user->user_Id,$input['flowId'],$input['document_Id'],$input['file'],$input['textProcess']);
+        dd($process);
+    }
+}
