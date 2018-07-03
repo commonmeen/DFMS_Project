04f114b90d13cb1e4fffa647c3998c59a2bbc585
NonRn <rn.non@hotmail.com>
2018-07-04 02:44:13 +0700

    [Edit] Controller FlowTemplateController | fixed FlowTemplate bugs.

--- app/Http/Controllers/AddFlowTemplateController.php
+++ app/Http/Controllers/AddFlowTemplateController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Session ;
 use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo ;
 use App\Repositories\Eloquent\EloquentUserRepository as userRepo ;
+use App\Repositories\Eloquent\EloquentStepRepository as stepRepo ;
 use App\Repositories\Eloquent\EloquentPositionRepository as positionRepo ;
 
 class AddFlowTemplateController extends Controller
@@ -15,17 +16,27 @@ class AddFlowTemplateController extends Controller
         if($request->has('flow_Id')){
             if ($request->has('template_Id')){
                 flowRepo::addFlowTemplate($input["flow_Id"],$input['template_Id']);
+            } else {
+                flowRepo::addFlowTemplate($input["flow_Id"],[]);
             }
+            Session::forget('FlowEdit');
             return redirect()->route('FlowDetail',['id'=>$input["flow_Id"]]);
         }else{
             $flow = Session::get('FlowCreate');
             if ($request->has('template_Id')){
                 flowRepo::addFlowTemplate($flow["flow_Id"],$input['template_Id']);
+            } else {
+                flowRepo::addFlowTemplate($flow["flow_Id"],[]);
             }
-            $next = 1 ;
+            $allStep = stepRepo::getStepByFlow($flow['flow_Id']);
+            $allStepId = array();
+            foreach($allStep as $step){
+                array_push($allStepId,$step['step_Id']);
+            }
+            $next = count($allStepId)+1 ;
             $allUser = userRepo::listUser();
             $position = positionRepo::getAllPosition();
-            return view('AddStep',['allStep'=>array(), 'step'=>$next,'userList'=>$allUser, 'userPosition'=>$position, 'flow'=>$flow, 'stepData'=>null]) ;
+            return view('AddStep',['allStep'=>$allStepId, 'step'=>$next,'userList'=>$allUser, 'userPosition'=>$position, 'flow'=>$flow, 'stepData'=>null]) ;
         }
     }
 }
