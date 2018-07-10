2c3215c3b6bb25478617d1f40c3605d8c3473a98
NonRn <rn.non@hotmail.com>
2018-07-11 03:21:57 +0700

    [Add] Controller EditFlowManageController

---
+++ app/Http/Controllers/EditFlowManageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session ;
+use App\Repositories\Eloquent\EloquentCategoryRepository as catRepo;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo;
+use App\Repositories\Eloquent\EloquentTemplateRepository as tempRepo;
+use App\Repositories\Eloquent\EloquentStepRepository as stepRepo;
+
+class EditFlowManageController extends Controller
+{
+    public function editFlow(Request $request){
+        if(Session::has('stepEdit')){
+            if(Session::get('stepEdit')['step_Title']=="")
+                stepRepo::deleteStep(Session::get('stepEdit')['step_Id']);
+            Session::forget('stepEdit');
+        }
+        $input = $request->all();
+        $cats = catRepo::getAllCategory();
+        $flow = flowRepo::getFlowById($input['flow_Id']);
+        $allTemplate = tempRepo::listTemplate();
+        $stepFlow = stepRepo::getStepByFlow($flow['flow_Id']);
+        Session::put('FlowEdit',$flow);
+        return view('EditFlow',['listCat'=>$cats,'flow'=>$flow,'template'=>$allTemplate,'step'=>$stepFlow]);
+    }
+}
