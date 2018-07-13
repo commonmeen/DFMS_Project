4fc5d102e676b1b678094e4d55d28cec68bfd1a6
NonRn <rn.non@hotmail.com>
2018-07-14 02:11:01 +0700

    [Edit] Controller FileUploadController | fixed bugs of upload file.

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -3,17 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session ;
+use Storage ;
 
 class FileUploadController extends Controller
 {
     public function upload(Request $request){
         $files = request()->file ;
+        $img = array();
         if($files!=null){
             foreach($files as $file){
                 $imageName = $file->getClientOriginalName();
+                for($i=1 ; Storage::disk('public')->exists('upload/'.$imageName) ; $i++){
+                    $imageName = $file->getClientOriginalName();
+                    $filename = pathinfo($imageName, PATHINFO_FILENAME);
+                    $extension = pathinfo($imageName, PATHINFO_EXTENSION);
+                    $imageName = $filename."(".$i.").".$extension;
+                }
                 $file->move(public_path('upload'), $imageName);   
+                array_push($img,$imageName);
             }
-            return response()->json(['uploaded' => '/upload/'.$imageName]) ;
+            Session::put("fileUploaded",$img);
+            return response()->json(['uploaded' => $img]) ;
         } else {
             return response()->json() ;
         }
