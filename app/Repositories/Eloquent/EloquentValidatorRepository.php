688736d894b24f043ec4aed6eb51f7cf6ba1deb6
NonRn <rn.non@hotmail.com>
2018-08-03 03:03:17 +0700

    [Add] Method addStepToValidator and removeStepFromValidator ; ValidatorRepository

--- app/Repositories/Eloquent/EloquentValidatorRepository.php
+++ app/Repositories/Eloquent/EloquentValidatorRepository.php
@@ -18,4 +18,27 @@ class EloquentValidatorRepository extends AbstractRepository implements Validato
         $validator = Validator::where('user_Id',$user_Id)->first();
         return json_decode($validator,true);
     }
+
+    public static function addStepToValidator($user_Id,$step){
+        $validator = Validator::where('user_Id',$user_Id)->first();
+        if($validator == null){
+            $validator = new Validator ;
+            $validator->user_Id = $user_Id ;
+            $validator->step_Id = [];
+        }
+        $array_step = $validator->step_Id ;
+        array_push($array_step,$step);
+        $validator->step_Id = $array_step ;
+        $validator->save();
+        return ;
+    }
+    public static function removeStepFromValidator($user_Id,$step){
+        $validator = Validator::where('user_Id',$user_Id)->first();
+        $array_step = $validator->step_Id ;
+        $key = array_search($step,$array_step);
+        array_forget($array_step,$key);
+        $validator->step_Id = $array_step ;
+        $validator->save();
+        return ;
+    }
 }
