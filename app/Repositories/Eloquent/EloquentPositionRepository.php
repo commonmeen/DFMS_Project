963cf6a6b4c35807ba2fc479e4ee710e46e2e634
NonRn <rn.non@hotmail.com>
2018-08-03 03:04:10 +0700

    [Add] Method addStepToPosition and removeStepFromPosition ; PositionRepository

--- app/Repositories/Eloquent/EloquentPositionRepository.php
+++ app/Repositories/Eloquent/EloquentPositionRepository.php
@@ -19,4 +19,22 @@ class EloquentPositionRepository extends AbstractRepository implements PositionR
         $data = Position::all();
         return json_decode($data);
     }
+
+    public static function addStepToPosition($position_Id,$step){
+        $position = Position::where('position_Id',$position_Id)->first();
+        $array_step = $position->validate_Step ;
+        array_push($array_step,$step);
+        $position->validate_Step = $array_step ;
+        $position->save();
+        return ;
+    }
+    public static function removeStepFromPosition($position_Id,$step){
+        $position = Position::where('position_Id',$position_Id)->first();
+        $array_step = $position->validate_Step ;
+        $key = array_search($step,$array_step);
+        array_forget($array_step,$key);
+        $position->validate_Step = $array_step ;
+        $position->save();
+        return ;
+    }
 }
