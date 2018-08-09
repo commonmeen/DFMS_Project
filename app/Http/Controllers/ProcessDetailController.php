6e035d08372f5a9bbe5315c02e00015c75caae3e
NonRn <rn.non@hotmail.com>
2018-08-10 02:04:33 +0700

    [Edit] Controller ProcessDetailController | Sent more data for more function.

--- app/Http/Controllers/ProcessDetailController.php
+++ app/Http/Controllers/ProcessDetailController.php
@@ -7,6 +7,7 @@ use App\Repositories\Eloquent\EloquentProcessRepository as processRepo ;
 use App\Repositories\Eloquent\EloquentStepRepository as stepRepo ;
 use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo ;
 use App\Repositories\Eloquent\EloquentDocumentRepository as docRepo ;
+use App\Repositories\Eloquent\EloquentUserRepository as userRepo ;
 
 class ProcessDetailController extends Controller
 {
@@ -16,12 +17,19 @@ class ProcessDetailController extends Controller
         $stepFlow = stepRepo::getStepByFlow($thisProcess['process_FlowId']);
         $flow = flowRepo::getFlowById($thisProcess['process_FlowId']);
         $thisProcess['process_FlowName'] = $flow['flow_Name'] ;
+        for ($i = 0 ; $i < count($thisProcess['process_Step']) ; $i++){
+            $thisProcess['process_Step'][$i]['approver_Detail'] = json_decode(userRepo::getUser($thisProcess['process_Step'][$i]['validator_Id']),true);
+        }
         $documentName = array();
         foreach ($thisProcess['data']['document_Id'] as $docId){
             $doc = docRepo::getDocumentById($docId);
             array_push($documentName,$doc['document_Name']);
         }
         $thisProcess['data']['document_Name'] = $documentName ;
-        return view('ProcessDetail',['process'=>$thisProcess,'steps'=>$stepFlow]);
+        $canApprove = false ;
+        if($request->has("InProgress")){
+            $canApprove = true ;
+        }
+        return view('ProcessDetail',['process'=>$thisProcess,'steps'=>$stepFlow,'canApprove'=>$canApprove]);
     }
 } 
