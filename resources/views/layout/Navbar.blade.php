1f593776854d3ac93f5222befe6d7499c7cc50f1
NonRn <rn.non@hotmail.com>
2018-09-04 23:42:33 +0700

    [Edit] View NavBar | logout button

--- resources/views/layout/Navbar.blade.php
+++ resources/views/layout/Navbar.blade.php
@@ -53,7 +53,7 @@
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item disable" href="#">Sign out</a>
+                <a class="dropdown-item disable" href="Logout">Sign out</a>
               </div>
           </div>
         </div>
