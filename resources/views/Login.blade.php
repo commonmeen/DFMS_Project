c0422ee5729658524bb64ddf225150654709c1b6
NonRn <rn.non@hotmail.com>
2018-09-04 23:40:07 +0700

    [Edit] View Login | now can use.

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -31,8 +31,7 @@
                 position: absolute;
                 border-radius: 100%;
                 
-              }
-                       
+              }            
     </style>
     
 
@@ -42,7 +41,8 @@
         <div class="row">
 
             <div class="col-lg-6 col-12 block-center block-top animated flipInX delay-5s">
-                <form name="loginform" id="loginform" method="POST" action="">
+                <form name="loginform" id="loginform" method="post" action="/">
+                    @csrf
                     <div class="row mb-2">
                         <div class="col-lg-12 center">
                             <label class="topic-login animated pulse infinite delay-3s">Sign in</label>
@@ -50,7 +50,7 @@
                     </div>
                     <div class="row mb-2">
                         <div class="col-lg-12">
-                            <input type="text" class="form-control" placeholder="Username" id="username" onkeyup="validateUser()">
+                            <input type="text" class="form-control" placeholder="Username" id="username" name="username">
                         </div>
                         <div class="col-lg-12">
                             <div id="err-username"></div>
@@ -59,7 +59,7 @@
                     
                     <div class="row mb-2">
                         <div class="col-lg-12">
-                            <input type="password" class="form-control" placeholder="Password" id="password" onkeyup="validatePassword()">
+                            <input type="password" class="form-control" placeholder="Password" id="password" name="password">
                         </div>
                         <div class="col-lg-12">
                             <div id="err-password"></div>
@@ -73,98 +73,13 @@
                 </div>
             </div>
         </div>
+        <p> @if($Err != null) {{$Err}}@endif</p>
     </div>
 
     <script>
-        //Validate form
         function submit(){
-            var user = validateUser();
-            var pass = validatePassword();
-            if(user&&pass == true){
-                document.getElementById('loginform').submit();
-                console.log('Login pass')
-            }
-            else
-                console.log('Login false');
+            document.getElementById('loginform').submit();
         }
-
-        function validateUser(){
-            user = document.getElementById('username').value;
-            if(user == ""){
-                document.getElementById('err-username').innerHTML = "Username is required";
-                document.getElementById('err-username').style.color = "red";
-                document.getElementById('err-username').style.fontWeight = "bold";
-                console.log('User : False');
-                return false;
-            }
-            else
-                document.getElementById('err-username').innerHTML = '';
-                console.log('User : True');
-                return true;
-        }
-
-        function validatePassword(){
-            pass = document.getElementById('password').value;
-            if(pass == ""){
-                document.getElementById('err-password').innerHTML = "Password is required";
-                document.getElementById('err-password').style.color = "red";
-                document.getElementById('err-password').style.fontWeight = "bold";
-                console.log('Pass : False');
-                return false;
-            }
-            else
-                document.getElementById('err-password').innerHTML = '';
-                console.log('Pass : True');
-                return true;
-        }
-
-
-
-
-
-
-        // Some random colors
-{{--  const colors = ["#3CC157", "#EE4266", "#1B1B1B", "#FCBC0F", "#F85F36" ,"#FFF"];
-const numBalls = 50;
-const balls = [];
-
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `${Math.random()}em`;
-  ball.style.height = ball.style.width;
-  
-  balls.push(ball);
-  document.body.append(ball);
-}
-
-// Keyframes
-balls.forEach((el, i, ra) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
-  };
-
-  let anim = el.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
-    ],
-    {
-      duration: (Math.random() + 1) * 2000, // random duration
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
-    }
-  );
-});
-
-    </script>  
-     --}}
+    </script>
 </body>
 </html>
