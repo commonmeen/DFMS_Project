f9aad955d799cba4d302c08d86768faff236e89c
NonRn <rn.non@hotmail.com>
2018-09-12 17:25:05 +0700

    [Edit] Controller MenuController | login with checkpassword method

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -19,23 +19,19 @@ class MenuController extends Controller
                 return view('Home',['data'=>$data,'catFlow'=>$catFlow]);
             }
         } else if($request->has('username')) {
-            $ldap = ldap_connect("13.229.128.241",389);
-            ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
-            $searchOnLdap=ldap_search($ldap, "dc=ldap,dc=doculdap,dc=tk", "mail=".$request->input('username'));
-            $userOnLdap = ldap_get_entries($ldap, $searchOnLdap);
-            if($userOnLdap['count']==0){
+            $passStatus = userRepo::checkPassword($request->input('username'),$request->input('password'));
+            if($passStatus == "None User"){
                 return view('Login')->with("Err","Don't have this E-mail on our organization, Please contact the administrator.");
-            }
-            if($userOnLdap[0]['userpassword'][0] == hash("sha256",$request->input('password'))){
-                $data = userRepo::getUser($userOnLdap[0]['uid'][0]);
+            } else if($passStatus == "Incorrect"){
+                return view('Login')->with("Err","Password incorrect, Please try agian.");
+            } else {
+                $data = userRepo::getUser($passStatus[0]['uid'][0]);
                 if($data!=null){
                     Session::put('UserLogin',$data);
                     return view('Home',['data'=>$data,'catFlow'=>$catFlow]);
                 } else {
                     return view('Login')->with("Err","Don't have user in database, Please contact the administrator.");
                 }
-            } else {
-                return view('Login')->with("Err","Password incorrect, Please try agian.");
             }
         } else {
             return view('Login')->with("Err","");
