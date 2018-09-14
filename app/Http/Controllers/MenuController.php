240eb26a7e799da1e771e72c76902728489799c3
commonmeen <commonmeen@gmail.com>
2018-09-14 15:42:00 +0700

    [Edit] Controller MenuController | merge code

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -19,27 +19,22 @@ class MenuController extends Controller
                 return view('Home',['data'=>$data,'catFlow'=>$catFlow]);
             }
         } else if($request->has('email')) {
-            $ldap = ldap_connect("13.229.128.241",389);
-            ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
-            $searchOnLdap=ldap_search($ldap, "dc=ldap,dc=doculdap,dc=tk", "mail=".$request->input('email'));
-            $userOnLdap = ldap_get_entries($ldap, $searchOnLdap);
-            if($userOnLdap['count']==0){
+            $passStatus = userRepo::checkPassword($request->input('email'),$request->input('password'));
+            if($passStatus == "None User"){
                 $errMessage = "Don't have this E-mail on our organization, Please contact the administrator.";
                 return view('Login', ['Err'=>$errMessage]);
-            }
-            if($userOnLdap[0]['userpassword'][0] == hash("sha256",$request->input('password'))){
-                $data = userRepo::getUser($userOnLdap[0]['uid'][0]);
-
-                if($data!=null){
-                    Session::put('UserLogin',$data);
-                    return view('Home',['data'=>$data,'catFlow'=>$catFlow]);
-                } else {
-                    $errMessage = "Don't have user in database, Please contact the administrator.";
-                    return view('Login',['Err'=>$errMessage]);
-                }
+            } else if($passStatus == "Incorrect"){
+                $errMessage = "Password incorrect, Please try agian.";
+                return view('Login', ['Err'=>$errMessage]);
             } else {
-                $errMessage = "Username or Password is incorrect, Please try agian.";
-                return view('Login',['Err'=>$errMessage]);
+                $data = userRepo::getUser($passStatus[0]['uid'][0]);
+                if($data != null){
+                    Session::put('UserLogin',$data);
+                    return view('Home',['data'=>$data, 'catFlow'=>$catFlow]);
+                } else{
+                    $errMessage = "Don't have user in database, Please contact the administrator.";
+                    return view('Login', ['Err'=>$errMessage]);  
+                }
             }
         }else {
             $errMessage = "";
