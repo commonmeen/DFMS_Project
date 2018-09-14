dd43a5e01c00b0297b82da0f1912fdfe13d411be
commonmeen <commonmeen@gmail.com>
2018-09-14 14:22:17 +0700

    [Add] View ListDocTemplate

--- resources/views/ListDocTemplate.blade.php
+++ resources/views/ListDocTemplate.blade.php
@@ -5,16 +5,47 @@
 @endsection
 @section('content')
     <div class="container " style="font-size:20pt">
-        <div id="fb-editor" class="block-top"></div>
+        <br>
+        <div class="row">
+            {{--  Large screen  --}}
+            <div class="col-12 col-sm-5 col-md-5 col-lg-6 d-none d-sm-block">   
+                <p class="topic">Document Template</p>
+            </div>
+            {{--  Small screen  --}}
+            <div class="col-12 d-sm-none center">   
+                <p class="topic">Document Template</p>
+            </div>            
+        </div>
+
+
+        <div id="build-wrap"></div>
+        <div id="fb-rendered-form">
+            <form action="#"></form>
+        </div>
     </div>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <script src="http://formbuilder.online/assets/js/form-builder.min.js"></script>
     <script>
-    jQuery(function($) {
-        $(document.getElementById('fb-editor')).formBuilder();
-    });
-    console.log(document.getElementById('fb-editor').value);
+              
+            jQuery(function($) {
+                var fbEditor = $(document.getElementById('build-wrap'));
+                var formContainer = $(document.getElementById('fb-rendered-form'));
+                var options = {
+                    onSave: function() {
+                        var $data = formBuilder.actions.getData('json');
+                        var $decode = jQuery.parseJSON($data);
+                        console.log($decode);
+                        
+                        fbEditor.toggle();
+                        formContainer.toggle();
+                        $('form', formContainer).formRender({
+                            formData: formBuilder.formData
+                        });
+                      }
+                };
+                var formBuilder = $(fbEditor).formBuilder(options);
+            });
     </script>
 @endsection
