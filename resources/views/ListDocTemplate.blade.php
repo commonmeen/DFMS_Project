a7d1f24f8aee735ef776f96388f5a03b6c33444c
NonRn <rn.non@hotmail.com>
2018-09-26 20:33:17 +0700

    [Edit] View ListDocTemplate | Data Table

--- resources/views/ListDocTemplate.blade.php
+++ resources/views/ListDocTemplate.blade.php
@@ -3,157 +3,90 @@
     {{Session::get('UserLogin')->user_Name}}
     {{Session::get('UserLogin')->user_Surname}}
 @endsection
+@section('script')
+<link rel="stylesheet" href="http://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"/>
+<script src="http://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready( function () {
+        $('#allTemp').DataTable();
+    } );
+</script>
+@endsection
 @section('content')
-    <div class="container " style="font-size:20pt">
-        <br>
-        <div class="row">
-            {{--  Large screen  --}}
-            <div class="col-12 col-sm-5 col-md-5 col-lg-6 d-none d-sm-block">   
-                <p class="topic">Create Document Template</p>
-            </div>
-            {{--  Small screen  --}}
-            <div class="col-12 d-sm-none center">   
-                <p class="topic">Create Document Template</p>
-            </div>            
+<div class="container">
+    <br>
+    <div class="row">
+        {{--  Large screen  --}}
+        <div class="col-12 col-sm-5 col-md-5 col-lg-6 d-none d-sm-block">   
+            <p class="topic">Template List</p>
         </div>
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="form-group mb-0">
-                    <label class="col-form-labelr align-self-center topic-nomal">Name : </label>
-                </div>
-            </div>
-            <div class="col-lg-9 mb-3">
-                <input type="text" name="name" id="name" onkeyup="" class="form-control" placeholder="Example: แบบฟอร์มการขอยืมอุปกรณ์" value="">
-            </div>
+        <div class="col-sm-6 col-md-6 col-lg-6 d-none d-sm-block">
+            <a role="button" class="btn btn-success float-right" href="AddDocTemplate">New Template</a>
         </div>
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="form-group mb-0">
-                    <label class="col-form-labelr align-self-center topic-nomal">Description : </label>
-                </div>
-            </div>
-            <div class="col-lg-9 mb-3">
-                <textarea id="desc" name="desc" class="form-control" placeholder="Emample: ใช้สำหรับสร้างเอกสารเพื่อขอยืมอุปกรณ์ในคณะเท่านั้น"></textarea>
-            </div>
+        {{--  Small screen  --}}
+        <div class="col-12 d-sm-none center">   
+            <p class="topic">Template List</p>
         </div>
-        <div id="build-wrap"></div>
-        <div class="fb-render"></div>
-        <center>
-            <button type="button" class="mt-3 btn-danger" id="clear">Clear</button>
-            <button type="button" class="mt-3 btn-success" id="save">Save</button>
-        </center>
+        <div class="col-12 d-sm-none ">
+            <a role="button" class="btn btn-block btn-success float-right" href="AddDocTemplate">New Template</a>
+        </div>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <script src="http://formbuilder.online/assets/js/form-builder.min.js"></script>
-    <script src="http://formbuilder.online/assets/js/form-render.min.js"></script>
-    <script>    
-        jQuery(function($) {
-            var fbEditor = $(document.getElementById('build-wrap'));
-            var fields = [
-                {
-                    label: "Time",
-                    type: "text",
-                    subtype: "time",
-                    icon: '⧖'
-                },{
-                    label: "Label",
-                    type: "header",
-                    icon: '𝒍'
-                },{
-                    label: "Text",
-                    type: "text"
-                },{
-                    label: "Radio Button",
-                    type: "radio-group"
-                },{
-                    label: "Checkbox",
-                    type: "checkbox-group"
-                },{
-                    label: "Dropdown",
-                    type: "select"
-                },{
-                    label: "Date",
-                    type: "date"
-                }
-            ];
-            var options = {
-                fields: fields,
-                subtypes: {
-                    text: ['time']
-                },
-                typeUserAttrs: {
-                    date: {
-                        min: {
-                            label: 'Date minimum',
-                            maxlength: '10',
-                            description: 'Minimum'
-                        },
-                        max: {
-                            label: 'Date max.',
-                            maxlength: '10',
-                            onclick: 'alert("wooohoooo")'
-                        }
-                    }
-                },
-                disabledAttrs: ["className", "access","name"],
-                typeUserDisabledAttrs: {
-                    'checkbox-group': [
-                        'toggle'
-                    ],
-                    'date':[
-                        'placeholder'
-                    ],
-                    'select':[
-                        'placeholder'
-                    ],
-                    'textarea':[
-                        'subtype'
-                    ]
-                },
-                disableFields: [
-                    'autocomplete',
-                    'date',
-                    'select',
-                    'checkbox-group',
-                    'button',
-                    'file',
-                    'paragraph',
-                    'hidden',
-                    'header',
-                    'text',
-                    'radio-group'
-                ],
-                showActionButtons: false
-            };
-            document.getElementById('clear').addEventListener('click', function() {
-                formBuilder.actions.clearFields();
-            });
-            document.getElementById('save').addEventListener('click', function() {
-                fbEditor.toggle();
-                formData = formBuilder.actions.getData('json');
-                data = {title : document.getElementById('name').value,desc : document.getElementById('desc').value,formData: formData}
-                console.log(formData);
-                // window.location='AddDocTemplate';
-                $.ajax({
-                    type     : "GET",
-                    url      : 'SaveDocTemplate',
-                    data     : data,
-                    cache    : false,
-                    success  : function(response){
-                        console.log("bye",response);
-                        $('.fb-render').formRender({
-                            dataType: 'json',
-                            formData: response.temp.template_Properties 
-                        });
-                    }
-                });
-                // $('.fb-render').formRender({
-                //     dataType: 'json',
-                //     formData: formBuilder.actions.getData('json')
-                // });
-            });
-            var formBuilder = $(fbEditor).formBuilder(options);
-        });
-    </script>
+    <ul class="nav nav-tabs " role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active toggle-nav" data-toggle="tab" href="#all">All Template</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link toggle-nav" data-toggle="tab" href="#trash">Trash</a>
+        </li>
+    </ul>
+
+    <div class="tab-content">
+        {{--  all tab  --}}
+        <div id="all" class="container tab-pane active"><br>
+            <table class="table table-list-search table-hover" id="allTemp">
+                <thead>
+                    <tr class="center">
+                        <th>Template name</th>
+                        <th>Author</th>
+                        <th>Last Update</th>
+                        <th><span hidden>Modify</span></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($allTemplate as $template)
+                        <tr onclick="window.location='TemplateDetail?temp_id={{$template['template_Id']}}';">
+                            <td><div class="text-over">{{$template['template_Name']}}</div></td>
+                            <td class="center">{{$template['template_AuthorName']}}</td>
+                            <td class="center">{{$template['created_at']}}</td>
+                            <td><input type="image" src="pic/writing.png" onclick="window.location='';" style="width:24px;height:24px;"/>&nbsp;&nbsp;&nbsp;<input type="image" src="pic/lock.png" onclick="" style="width:24px;height:24px;"/></td>
+                        </tr>
+                    @endforeach              
+                </tbody>
+            </table>
+        </div>
+
+        {{--  trash tab  --}}
+        <div id="trash" class="container tab-pane fade"><br>
+            <table class="table table-list-search table-hover" id="success-page">   
+                <thead>
+                    <tr class="center">
+                        <th>Process name</th>
+                        <th>Date/Time</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {{-- @foreach($successProcess as $process)
+                        @php $step = count($process['process_Step'])@endphp
+                        <tr onclick="window.location='ProcessDetail?id={{$process['process_Id']}}';">
+                            <td><div class="text-over">{{$process['process_Name']}}</div></td>
+                            <td class="center">{{$process['created_at']}}</td>
+                            <td class="center">{{ucfirst($process['current_StepId'])}}</td>
+                        </tr>
+                    @endforeach --}}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 @endsection
