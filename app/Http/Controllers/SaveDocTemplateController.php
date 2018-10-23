a879036b1074eb118eecfd4d84cd7743a66bf4f2
commonmeen <commonmeen@gmail.com>
2018-10-23 14:23:45 +0700

    [Edit] Controller SaveDocTemplateController | separate call function between 'Add' and 'Edit' template

--- app/Http/Controllers/SaveDocTemplateController.php
+++ app/Http/Controllers/SaveDocTemplateController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Session ;
 use App\Repositories\Eloquent\EloquentTemplateRepository as templateRepo ;
-
 class SaveDocTemplateController extends Controller
 {
     public function saveTemplate(Request $request){
         if(Session::has('UserLogin') && Session::get('UserLogin')->user_Role=="manager"){
-            $temp = templateRepo::addTemplate($request->input('title'),Session::get('UserLogin')->user_Id,$request->input('desc'),$request->input('formData'));
-            return ['temp'=>$temp];
+            $session = Session::get('tempStatus');
+            if($session == 'AddTemplate'){
+                $temp = templateRepo::addTemplate($request->input('title'),Session::get('UserLogin')->user_Id,$request->input('desc'),$request->input('formData'));
+                return ['temp'=>$temp];
+            }
+            else{
+                $data = $request->all();
+                $data['template_Id'] = Session::get('tempStatus');
+                $temp = templateRepo::editTemplate($data);
+                Session::forget('tempStatus');
+                return ['temp'=>$temp];
+            }
         }
     }
 }
