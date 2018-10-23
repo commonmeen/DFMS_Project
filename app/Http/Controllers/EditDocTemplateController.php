b953f38122a669d32e4fe06b09416fc25e7a0b8f
commonmeen <commonmeen@gmail.com>
2018-10-23 14:10:32 +0700

    [Edit] Controller EditDocTemplateController

--- app/Http/Controllers/EditDocTemplateController.php
+++ app/Http/Controllers/EditDocTemplateController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repositories\Eloquent\EloquentTemplateRepository as tempRepo;
 use App\Repositories\Eloquent\EloquentUserRepository as userRepo;
-use Debugbar;
 
 class EditDocTemplateController extends Controller
 {
@@ -13,7 +12,6 @@ class EditDocTemplateController extends Controller
         $input = $request->all();
         $data = tempRepo::getTemplateById($input['temp_id']);
         $data->template_Owner = userRepo::getUser($data->template_Author);
-        Debugbar::info($data);
         return view('EditDocTemplate',['data'=>$data]);
     }
 }
