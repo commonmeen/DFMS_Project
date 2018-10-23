63e9942a7c5a8e82795480c3aff1f292e035df0c
NonRn <rn.non@hotmail.com>
2018-10-23 23:30:41 +0700

    [Edit] Controller AddDocumentController

--- app/Http/Controllers/AddDocumentController.php
+++ app/Http/Controllers/AddDocumentController.php
@@ -11,8 +11,23 @@ class AddDocumentController extends Controller
 {
     public function addDoc(Request $request){
         if(Session::has('UserLogin')){
-            $tempCanUse = tempRepo::listOnTemplate();
-            return view("AddDocument",['template'=>$tempCanUse]);
+            $input =   $request->all();
+            $prop = json_decode($input['prop']);
+            $datas = array();
+            $nameOfProp = array_keys($input);
+            foreach($prop as $p){
+                $data = array();
+                foreach($nameOfProp as $name){
+                    if($p->type != "header" && $p->name == $name){
+                        $data['title'] = $p->label ;
+                        $data['detail'] = $input[$p->name] ;
+                        array_push($datas,$data) ;
+                        break ;
+                    }
+                }
+            }
+            $newDocumentId = docRepo::addNewDocument($input['name'],Session::get('UserLogin')->user_Id,$input['tempId'],$datas);
+            return redirect('DocumentDetail?doc_Id='.$newDocumentId);
         } else {
             return view('Login');
         }
