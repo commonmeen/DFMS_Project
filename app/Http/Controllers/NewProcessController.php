380768d55a9f93e4faaf271347b2299efee2c2df
NonRn <rn.non@hotmail.com>
2018-10-26 17:07:38 +0700

    [Edit] Controller ProcessDetail | doc status change

--- app/Http/Controllers/NewProcessController.php
+++ app/Http/Controllers/NewProcessController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Session ;
 use App\Repositories\Eloquent\EloquentProcessRepository as processRepo ;
+use App\Repositories\Eloquent\EloquentDocumentRepository as docRepo ;
 
 class NewProcessController extends Controller
 {
@@ -17,6 +18,9 @@ class NewProcessController extends Controller
             else
                 $fileName = Session::get('fileUploaded');
             $process = processRepo::newProcess($input['name'],$user->user_Id,$input['flowId'],$input['document_Id'],$fileName,$input['textProcess']);
+            foreach($input['document_Id'] as $doc_Id){
+                docRepo::changeStatus($doc_Id,"used");
+            }
             Session::forget('fileUploaded');
             return redirect('ListProcess');
         } else {
