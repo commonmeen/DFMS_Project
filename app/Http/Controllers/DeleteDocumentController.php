7523775cdc25293411cb9e1ff25ea7c2083c8371
commonmeen <commonmeen@gmail.com>
2018-10-26 16:25:09 +0700

    [Edit] Controller DeleteDocumentController | delete debugbar

--- app/Http/Controllers/DeleteDocumentController.php
+++ app/Http/Controllers/DeleteDocumentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\Eloquent\EloquentDocumentRepository as documentRepo;
-use Debugbar;
 use Session;
 
 class DeleteDocumentController extends Controller
@@ -12,7 +11,6 @@ class DeleteDocumentController extends Controller
     public function deleteDocument(Request $request){
         if(Session::has('UserLogin') && Session::get('UserLogin')->user_Role=="manager"){
             $input = $request->all();
-            Debugbar::info($input);
             $data = documentRepo::deleteDocumentById($input['document_Id']);
         } else {
             dd("Error occur", "Permission denied. Plz login on manager role.");
