1bdb2aac6bcf5ea9444b50ecc71cd1db788c89b3
NonRn <rn.non@hotmail.com>
2018-10-28 15:10:31 +0700

    [Edit] Controller MenuController | fixed approve by position bug.

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -9,6 +9,7 @@ use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo;
 use App\Repositories\Eloquent\EloquentProcessRepository as processRepo;
 use App\Repositories\Eloquent\EloquentValidatorRepository as validatorRepo;
 use App\Repositories\Eloquent\EloquentStepRepository as stepRepo;
+use App\Repositories\Eloquent\EloquentPositionRepository as positionRepo;
 
 class MenuController extends Controller
 {
@@ -26,37 +27,42 @@ class MenuController extends Controller
             }
             // Verify process
             $now = array();
-            $owner = array();
+            $allStepData = array();
+            $position_Id = $data->user_Position ;
+            foreach($position_Id as $pos_Id){
+                $position = positionRepo::getPositionById($pos_Id);
+                foreach($position['validate_Step'] as $vaStep_Id){
+                    array_push($allStepData,stepRepo::getStepById($vaStep_Id));
+                }
+            }
+            $allProcessCanApprove = array();
             if($validator != null){
-                $allProcessCanApprove = array();
-                $allStepData = array();
                 foreach($validator['step_Id'] as $stepId){
                     array_push($allStepData,stepRepo::getStepById($stepId));
                 }
-            
-                foreach($allStepData as $stepData){
-                    $allProcessCanApprove = array_add($allProcessCanApprove,$stepData['flow_Id'],processRepo::getProcessByFlow($stepData['flow_Id']));
-                }
-                $allProcessCanApprove = array_collapse($allProcessCanApprove);
-                foreach($allProcessCanApprove as $process){
-                    $flowOnProcess = flowRepo::getFlowById($process['process_FlowId']);
-                    $processOwner = userRepo::getUser($process['process_Owner']);
+            }
+            foreach($allStepData as $stepData){
+                $allProcessCanApprove = array_add($allProcessCanApprove,$stepData['flow_Id'],processRepo::getProcessByFlow($stepData['flow_Id']));
+            }
+            $allProcessCanApprove = array_collapse($allProcessCanApprove);
+            foreach($allProcessCanApprove as $process){
+                $flowOnProcess = flowRepo::getFlowById($process['process_FlowId']);
+                $processOwner = userRepo::getUser($process['process_Owner']);
 
-                    $process['flowObject'] = $flowOnProcess ;
-                    $process['owner'] = $processOwner->user_Name.' '.$processOwner->user_Surname ;
-                    // dd($process);
-                    
-                    foreach($validator['step_Id'] as $step_Id){
-                        if($step_Id == $process['current_StepId']){
-                            array_push($now,$process);
-                            break ;
-                        }
+                $process['flowObject'] = $flowOnProcess ;
+                $process['owner'] = $processOwner->user_Name.' '.$processOwner->user_Surname ;
+                // dd($process);
+                
+                foreach($allStepData as $step){
+                    if($step['step_Id'] == $process['current_StepId']){
+                        array_push($now,$process);
+                        break ;
                     }
                 }
             }
-
+            $data = userRepo::getPosition($data);
             if($data!=null){
-                return view('Home',['data'=>$data,'catFlow'=>$catFlow, 'allProcess'=>$processes ,'nowProcess'=>$now, 'owner'=>$owner]);
+                return view('Home',['data'=>$data,'catFlow'=>$catFlow, 'allProcess'=>$processes ,'nowProcess'=>$now]);
             }
         } else if($request->has('email')) {
             $passStatus = userRepo::checkPassword($request->input('email'),$request->input('password'));
