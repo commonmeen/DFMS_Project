2c7faadc46af3e66093b1cd673ddec56d5bb3c8b
NonRn <rn.non@hotmail.com>
2018-11-05 19:02:35 +0700

    [Edit] View AddDocument | support edit document

--- resources/views/AddDocument.blade.php
+++ resources/views/AddDocument.blade.php
@@ -42,9 +42,9 @@
         margin-bottom: 20px !important;
     }
 </style>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <script src="https://formbuilder.online/assets/js/form-render.min.js"></script>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/jquery-ui.min.js"></script>
+    <script src="/js/form-render.min.js"></script>
     <script type="text/javascript">
         function templateSelect(){
             $(".forFormRender").html("");
@@ -69,7 +69,7 @@
             if(document.getElementById("name").value==""){
                 var isNotErr = false ;
                 document.getElementById("name").style.borderColor = "red" ;
-                document.getElementById("errname").innerHTML = "Please enter name of process" ;
+                document.getElementById("errname").innerHTML = "Please enter name of document" ;
             } else {
                 var isNotErr = true ;
                 document.getElementById("errname").innerHTML = "" ;
@@ -110,6 +110,16 @@
         function closeRequest(){
             return "You have unsaved changes!";
         }
+        
+        $(document).ready(function() {
+            if(document.getElementById('tempId').value != 'notSelect'){
+                templateSelect();
+                document.getElementById('hide').style.display = '';
+                document.getElementById('search').disabled = true ;
+                document.getElementById('topic1').innerHTML = "Edit Document" ;
+                document.getElementById('topic2').innerHTML = "Edit Document" ;
+            }
+        });
     </script>
 @endsection
 
@@ -124,17 +134,17 @@
         <div class="row">
             {{--  Large screen  --}}
             <div class="col-12 d-none d-sm-block">
-                <p class="topic">New Document</p>
+                <p class="topic" id="topic1">New Document</p>
             </div>
             {{--  Small screen  --}}
             <div class="col-12 center d-sm-none">
-                <p class="topic">New Document</p>
+                <p class="topic" id="topic2">New Document</p>
             </div>
     
             <br>
             <div class="col-12 col-lg-8 mb-2 block-center">
                 <span class="topic-nomal">Document name : </span>
-                <input type="text" name="name" id="name" class="form-control" onkeyup="nameValidate()" placeholder="Enter Document Name">
+                <input type="text" name="name" id="name" class="form-control" onkeyup="nameValidate()" placeholder="Enter Document Name" value="{{$documentData['document_Name']}}">
                 <div id="errname" class="err-text"></div>
             </div>
             <div class="col-12 col-lg-8 block-center">
@@ -144,7 +154,11 @@
                         <select class="form-control" name="tempId" id="tempId" onchange="templateSelect()">
                             <option id="defaultNull" disabled selected value="notSelect">Choose Template</option>
                             @for($i=0 ; $i<count($template) ; $i++)
-                                <option value="{{$template[$i]['template_Id']}}">{{$template[$i]['template_Name']}}</option>
+                                @if($documentData['document_TemplateId']==$template[$i]['template_Id'])
+                                    <option value="{{$template[$i]['template_Id']}}" selected>{{$template[$i]['template_Name']}}</option>
+                                @else
+                                    <option value="{{$template[$i]['template_Id']}}">{{$template[$i]['template_Name']}}</option>
+                                @endif
                             @endfor                         
                         </select>
                     </div>
