6590103f167a4ce8603f95a4b96e124f516e16d0
Meen <ppmeen.gamer@gmail.com>
2018-11-07 15:43:09 +0700

    [Add] View ApproveEmail | use for email sender

---
+++ resources/views/ApproveEmail.blade.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" /><!--[if !mso]><!-->
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" /><!--<![endif]-->
+    <!--[if (gte mso 9)|(IE)]>
+    <xml>
+    <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+    </xml>
+    <![endif]-->
+    <!--[if (gte mso 9)|(IE)]>
+    <style type="text/css">
+      body {width: 700px;margin: 0 auto;}
+      table {border-collapse: collapse;}
+      table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
+      img {-ms-interpolation-mode: bicubic;}
+    </style>
+    <![endif]-->
+
+    <style type="text/css">
+      body, p, div {
+        font-family: arial;
+        font-size: 14px;
+      }
+      body {
+        color: #000000;
+      }
+      body a {
+        color: #1188E6;
+        text-decoration: none;
+      }
+      p { margin: 0; padding: 0; }
+      table.wrapper {
+        width:100% !important;
+        table-layout: fixed;
+        -webkit-font-smoothing: antialiased;
+        -webkit-text-size-adjust: 100%;
+        -moz-text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+      }
+      img.max-width {
+        max-width: 100% !important;
+      }
+      .column.of-2 {
+        width: 50%;
+      }
+      .column.of-3 {
+        width: 33.333%;
+      }
+      .column.of-4 {
+        width: 25%;
+      }
+      @media screen and (max-width:480px) {
+        .preheader .rightColumnContent,
+        .footer .rightColumnContent {
+            text-align: left !important;
+        }
+        .preheader .rightColumnContent div,
+        .preheader .rightColumnContent span,
+        .footer .rightColumnContent div,
+        .footer .rightColumnContent span {
+          text-align: left !important;
+        }
+        .preheader .rightColumnContent,
+        .preheader .leftColumnContent {
+          font-size: 80% !important;
+          padding: 5px 0;
+        }
+        table.wrapper-mobile {
+          width: 100% !important;
+          table-layout: fixed;
+        }
+        img.max-width {
+          height: auto !important;
+          max-width: 480px !important;
+        }
+        a.bulletproof-button {
+          display: block !important;
+          width: auto !important;
+          font-size: 80%;
+          padding-left: 0 !important;
+          padding-right: 0 !important;
+        }
+        .columns {
+          width: 100% !important;
+        }
+        .column {
+          display: block !important;
+          width: 100% !important;
+          padding-left: 0 !important;
+          padding-right: 0 !important;
+          margin-left: 0 !important;
+          margin-right: 0 !important;
+        }
+      }
+    </style>
+    <!--user entered Head Start-->
+    
+     <!--End Head user entered-->
+  </head>
+  <body>
+    <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size: 14px; font-family: arial; color: #000000; background-color: #f1fbfb;">
+      <div class="webkit">
+        <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#f1fbfb">
+          <tr>
+            <td valign="top" bgcolor="#f1fbfb" width="100%">
+              <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+                <tr>
+                  <td width="100%">
+                    <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                      <tr>
+                        <td>
+                          <!--[if mso]>
+                          <center>
+                          <table><tr><td width="700">
+                          <![endif]-->
+                          <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width: 100%; max-width:700px;" align="center">
+                            <tr>
+                              <td role="modules-container" style="padding: 20px 30px 20px 30px; color: #000000; text-align: left;" bgcolor="#ffffff" width="100%" align="left">
+                                
+    <table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%"
+           style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+      <tr>
+        <td role="module-content">
+          <p>Document Submission waiting for your approval is going to timeout. [No Reply]</p>
+        </td>
+      </tr>
+    </table>
+  
+    <table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;">
+      <tr>
+        <td style="padding:0px 0px 0px 0px;"
+            height="100%"
+            valign="top"
+            bgcolor="">
+            <div style="text-align: right;">
+  <span style="font-size:12px;">Ours website 
+    <a href="www.dfms.cf"> www.dfms.cf </a>
+  </span>
+</div>
+        </td>
+      </tr>
+    </table>
+  <table class="module" role="module" data-type="code" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;">
+      <tr>
+        <td height="100%" valign="top">
+          <h1 class="p1" style="text-align: left;"><span style="font-size:20px;"><strong>Dear, {{$data->validator['user_Name']}} {{$data->validator['user_Surname']}}</strong></span></h1>
+  <div class="p1"><span style="font-size:15px;line-height:150%;">You have Document Submission waiting for your approval. As follow:
+	<br>Process flow: {{$data->flow_Name['flow_Name']}}.
+	<br>Owner: {{$data->owner->user_Name}} {{$data->owner->user_Surname}}.
+  </div>
+        </td>
+      </tr>
+    </table><table border="0" cellPadding="0" cellSpacing="0" class="module" data-role="module-button" data-type="button" role="module" style="table-layout:fixed" width="100%"><tbody><tr><td align="center" class="outer-td" style="padding:10px 0px 30px 0px"><table border="0" cellPadding="0" cellSpacing="0" class="button-css__deep-table___2OZyb wrapper-mobile" style="text-align:center"><tbody><tr><td align="center" bgcolor="#2a9ebf" class="inner-td" style="-webkit-border-radius:10px;-moz-border-radius:10px;border-radius:10px;font-size:16px;text-align:center;background-color:inherit"><a style="background-color:#2a9ebf;height:30px;width:200px;font-size:16px;line-height:25px;font-family:Helvetica, Arial, sans-serif;color:#ffffff;padding:12px 18px 12px 18px;text-decoration:none;-webkit-border-radius:10px;-moz-border-radius:10px;border-radius:10px;border:1px solid #2a9ebf;display:inline-block" href="http://127.0.0.1:8000/ProcessDetail?id={{$data->process_Id}}&InProgress=true" target="_blank">Click to approve</a></td></tr></tbody></table></td></tr></tbody></table>
+    <table class="module"
+           role="module"
+           data-type="spacer"
+           border="0"
+           cellpadding="0"
+           cellspacing="0"
+           width="100%"
+           style="table-layout: fixed;">
+      <tr>
+        <td style="padding:0px 0px 30px 0px;"
+            role="module-content"
+            bgcolor="">
+        </td>
+      </tr>
+    </table>
+  <table class="module" role="module" data-type="code" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;">
+      <tr>
+        <td height="100%" valign="top">
+          <div class="p1"><span style="font-size:14px;line-height:120%;">If you have any question or feedback, please contact us at : dfms.sit@gmail.com 
+  <br><br>Thank you for join use.
+  <br>The DFMS Team
+  </span>
+</div>
+  <div>&nbsp;</div>
+  <div><span style="font-size:12px;">Notes: Please do not respond directly to this e-mail, as it has been generated by an automated system.</span>
+  </div>
+        </td>
+      </tr>
+    </table>
+    <table class="module"
+           role="module"
+           data-type="spacer"
+           border="0"
+           cellpadding="0"
+           cellspacing="0"
+           width="100%"
+           style="table-layout: fixed;">
+      <tr>
+        <td style="padding:0px 0px 40px 0px;"
+            role="module-content"
+            bgcolor="">
+        </td>
+      </tr>
+    </table>
+  
+    <table class="module"
+           role="module"
+           data-type="spacer"
+           border="0"
+           cellpadding="0"
+           cellspacing="0"
+           width="100%"
+           style="table-layout: fixed;">
+      <tr>
+        <td style="padding:0px 0px 04px 0px;"
+            role="module-content"
+            bgcolor="#2A9EBF">
+        </td>
+      </tr>
+    </table>
+  
+    <table class="module"
+           role="module"
+           data-type="spacer"
+           border="0"
+           cellpadding="0"
+           cellspacing="0"
+           width="100%"
+           style="table-layout: fixed;">
+      <tr>
+        <td style="padding:0px 0px 10px 0px;"
+            role="module-content"
+            bgcolor="">
+        </td>
+      </tr>
+    </table>
+  <table class="module" role="module" data-type="code" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;">
+      <tr>
+        <td height="100%" valign="top">
+          <div style="font-size:12px;line-height:150%;margin:0;text-align:center;">This email was sent by: DFMS</div><br>
+<div style="font-size:12px;line-height:150%;margin:0;text-align:center;">School of Information Technology,&nbsp;King Mongkut's University of Technology Thonburi</div>
+        </td>
+      </tr>
+    </table>
+                              </td>
+                            </tr>
+                          </table>
+                          <!--[if mso]>
+                          </td></tr></table>
+                          </center>
+                          <![endif]-->
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </center>
+  </body>
+</html>
