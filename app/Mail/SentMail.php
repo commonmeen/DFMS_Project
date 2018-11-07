940b6447f040009cd44ab179f9e3dc7d35d6b74c
Meen <ppmeen.gamer@gmail.com>
2018-11-07 15:42:34 +0700

    [Add] Mailable Mail | email provider

---
+++ app/Mail/SentMail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Debugbar;
+
+
+class SentMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $address = 'dfms.sit@gmail.com';
+        $subject = 'Welcome to Document Flow Management System';
+        $name = 'Document Flow Management System';
+        Debugbar::info($this->data);
+        return $this->view('ApproveEmail',['data'=>$this->data])
+                    ->from($address, $name)
+                    ->cc($address, $name)
+                    ->bcc($address, $name)
+                    ->replyTo($address, $name)
+                    ->subject($subject);
+                    
+    }
+}
