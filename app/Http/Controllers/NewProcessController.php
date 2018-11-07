9accccba3d2cdb9011ebf710c4c4a88345f3c958
commonmeen <commonmeen@gmail.com>
2018-11-07 18:19:20 +0700

    [Edit] Controller NewProcessController | send email to first validator

--- app/Http/Controllers/NewProcessController.php
+++ app/Http/Controllers/NewProcessController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use Session ;
 use App\Repositories\Eloquent\EloquentProcessRepository as processRepo ;
 use App\Repositories\Eloquent\EloquentDocumentRepository as docRepo ;
+use App\Repositories\Eloquent\EloquentStepRepository as stepRepo ;
+use App\Repositories\Eloquent\EloquentUserRepository as userRepo ;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo ;
 
 class NewProcessController extends Controller
 {
@@ -21,6 +24,33 @@ class NewProcessController extends Controller
             foreach($input['document_Id'] as $doc_Id){
                 docRepo::changeStatus($doc_Id,"used");
             }
+            $processObject = json_decode($process);
+            $stepObject = stepRepo::getStepById($processObject->current_StepId);
+            
+            
+            if($stepObject['typeOfValidator'] == 'position'){
+                $validatorPositionId = $stepObject['validator'][0];
+                $data = $processObject;
+                $data->validator = json_decode(userRepo::getUserByPosition($validatorPositionId),true);
+                $data->owner = json_decode(userRepo::getUser($data->process_Owner));
+                $data->flow_Name = flowRepo::getFlowById($data->process_FlowId);
+                $validators = $data->validator;
+                
+                for($i=0;$i<count($validators);$i++){
+                    $data->validator = $validators[$i];
+                    userRepo::sentEmail($data,$data->validator['user_Email']);
+                }
+            }
+            else{
+                for($i=0;$i<count($stepObject['validator']);$i++){
+                    $data = $processObject;
+                    $data->validator = json_decode(userRepo::getUser($stepObject['validator'][$i]),true);
+                    $data->owner = json_decode(userRepo::getUser($data->process_Owner));
+                    $data->flow_Name = flowRepo::getFlowById($data->process_FlowId);
+                    userRepo::sentEmail($data,$data->validator['user_Email']);
+                }
+            }
+
             Session::forget('fileUploaded');
             return redirect('ListProcess');
         } else {
