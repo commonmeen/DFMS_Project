be4e0fba6bb50c8cd42b81a2e97c457f58ce09be
commonmeen <commonmeen@gmail.com>
2018-11-10 22:26:04 +0700

    [Edit] Controller RejectProcessController | update sent email when process is rejected

--- app/Http/Controllers/RejectProcessController.php
+++ app/Http/Controllers/RejectProcessController.php
@@ -5,15 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Session ;
 use App\Repositories\Eloquent\EloquentProcessRepository as processRepo;
+use App\Repositories\Eloquent\EloquentUserRepository as userRepo;
+use App\Repositories\Eloquent\EloquentFlowRepository as flowRepo;
 
 class RejectProcessController extends Controller
 {
     public function rejectProcess(Request $request){
         if(Session::has('UserLogin') && Session::get('UserLogin')->user_Role=="manager"){
+            $mailReject = 'reject'; 
             $input = $request->all();
             $user = Session::get('UserLogin');
+
             processRepo::reject($input['pid'],$input['sid'],$user->user_Id,$input['comment']);
-            return ;
+
+            $data = processRepo::getProcessById($input['pid']);
+            $data['process_Owner'] = userRepo::getUser($data['process_Owner']);
+            $data['flow'] = flowRepo::getFlowById($data['process_FlowId']);
+            $data['email_Type'] = $mailReject;
+            $data = (object) $data;
+            
+            return userRepo::sentEmail($data,$data->process_Owner['user_Email']);
         } else {
             dd("Error occur", "Permission denied. Plz login on manager role.");
         }
