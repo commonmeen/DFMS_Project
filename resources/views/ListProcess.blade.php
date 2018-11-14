b1ccc7a81dc62b93c4170da40ee07935d3921fee
commonmeen <commonmeen@gmail.com>
2018-11-14 13:21:31 +0700

    [Edit] View ListProcess | edit variable to alert status

--- resources/views/ListProcess.blade.php
+++ resources/views/ListProcess.blade.php
@@ -15,7 +15,7 @@
 
         {{--  Success alert  --}}
         {{--  flash session  --}}
-        @if(session('approveStatus') == 'Success')
+        @if(session('alertStatus') == 'Success')
         <div class="alert alert-success" id="success-alert">
             <button type="button" class="close" data-dismiss="alert">x</button>
             <strong>Success! </strong>
