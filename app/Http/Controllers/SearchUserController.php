eebe0dcb89efbb37bfcd24ad4588cd574b4ce670
NonRn <rn.non@hotmail.com>
2018-11-18 02:31:33 +0700

    [Edit] Controller SearchUserController | fixed check box bug.

--- app/Http/Controllers/SearchUserController.php
+++ app/Http/Controllers/SearchUserController.php
@@ -10,11 +10,26 @@ class SearchUserController extends Controller
 {
     public function searchUser(Request $request){
         $input = $request->all();
+        $checked = array();
+        if($request->has('checked')){
+            foreach($input['checked'] as $id_check){
+                array_push($checked,userRepo::getPosition(userRepo::getUser($id_check)));
+            }
+        }
         $name = userRepo::searchByName($input['search']);
         $surname = userRepo::searchBySurname($input['search']);
         $all = array_collapse([$name,$surname]);
         $all = array_unique($all,0);
-        return ['searchAll'=>$all] ;
+        foreach($checked as $user_check){
+            foreach($all as $user_search){
+                $i = array_search($user_check->user_Id,$user_search);
+                $key = array_keys($all,$user_search);
+                if($i == "user_Id"){
+                    array_splice($all,$key[0],1);
+                }
+            }
+        }
+        return ['searchAll'=>$all,'checked'=>$checked] ;
     }
 
     public function searchPosition(Request $request){
